64a668db36abb5c50305d283d88a929ae5c6beb0
Igor Pucci <i.pucci@list-group.com>
2025-02-03 14:15:47 +0100

    added reconnection capability

--- src/main/java/org/example/MKTBusSender.java
+++ src/main/java/org/example/MKTBusSender.java
@@ -5,15 +5,33 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.beans.ExceptionListener;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.TimerTask;
 import java.util.concurrent.TimeoutException;
+import com.google.common.base.Strings;
 
 import org.example.Market.QuoteRequest;
 //import org.example.Market.QuoteRequestLeg;
 import org.example.Market.QuoteResponse;
 import org.example.Market.Quote;
+import org.example.Utils;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MKTBusSender {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MKTBusSender.class);
+
     private final static String QUOTEREQ_QUEUE_NAME = "quotereq_mkt_queue";
     private final static String QUOTERES_QUEUE_NAME = "quoteres_mkt_queue";
     private final static String QUOTE_QUEUE_NAME = "quote_mkt_queue";
@@ -22,12 +40,24 @@ public class MKTBusSender {
     private final static String MISSING_CONNECTION = "Missing Connection";
     private final static String MISSING_CHANNEL = "Missing Channel";
     private final static String SENT_OK = "OK";
-    private final static Logger LOG = LoggerFactory.getLogger(MKTBusSender.class);
     private final static String EXCHANGE_NAME = "MKT";
+    private static final long MAX_CONNECTION_ATTEMPTS = 50;
 
-    private final ConnectionFactory busFactory;
-    private final Connection busConnection;
-    private final Channel busChannel;
+    private ConnectionFactory busFactory;
+    private Connection busConnection;
+    private Channel busChannel;
+
+    private String hostName;
+    private String virtualHost;
+    private int port;
+    private String userName;
+    private String password;
+
+    private static int connectionAttempts = 0;
+
+    private static volatile boolean isConnecting;
+    private static volatile boolean isConnected;
+
 
     /*
      
@@ -43,15 +73,15 @@ public class MKTBusSender {
                 .build();
      */
 
-    public MKTBusSender(String hostName, String virtualHost, int port, String userName, String password) throws IOException, TimeoutException{
-
-        // Set up the connection and channel
-        busFactory = new ConnectionFactory();
-        busFactory.setUsername(userName);
-		busFactory.setPassword(password);
-		busFactory.setVirtualHost(virtualHost);
-		busFactory.setHost(hostName);
-		busFactory.setPort(port);
+    public MKTBusSender(String hostName, String virtualHost, int port, String userName, String password) 
+    {
+        this.hostName = hostName;
+        this.virtualHost = virtualHost;
+        this.port = port;
+        this.password = password;
+        connectionAttempts = 0;
+        isConnecting = false;
+        isConnected = false;
 		/*
 		Property	Default Value
 		Username	"guest"
@@ -61,20 +91,77 @@ public class MKTBusSender {
 		port	5672 for regular connections, 5671 for connections that use TLS
 		*/
 
-        busConnection = busFactory.newConnection();
-        busChannel = busConnection.createChannel();
-        busChannel.exchangeDeclare(EXCHANGE_NAME, "topic");
-        busChannel.queueDeclare(QUOTEREQ_QUEUE_NAME, false, false, false, null);
-        busChannel.queueDeclare(QUOTERES_QUEUE_NAME, false, false, false, null);
-        busChannel.queueDeclare(QUOTE_QUEUE_NAME, false, false, false, null);
-        busChannel.queueDeclare(TRADE_QUEUE_NAME, false, false, false, null);
-        busChannel.queueDeclare(PRICE_QUEUE_NAME, false, false, false, null);
-        
-        busChannel.queueBind(QUOTEREQ_QUEUE_NAME, EXCHANGE_NAME, "QUOTEREQ.BOND.*.*");
-        busChannel.queueBind(QUOTE_QUEUE_NAME, EXCHANGE_NAME, "QUOTE.BOND.*.*");
-        busChannel.queueBind(QUOTERES_QUEUE_NAME, EXCHANGE_NAME, "QUOTERES.BOND.*.*");
-        busChannel.queueBind(TRADE_QUEUE_NAME, EXCHANGE_NAME, "TRADE.BOND.*.*");
-        busChannel.queueBind(PRICE_QUEUE_NAME, EXCHANGE_NAME, "PRICE.BOND.#");
+    }
+
+    public boolean connect()
+    {
+        try {
+            isConnecting =true;
+            LOG.info("creating bus factory {}@{}", userName, hostName);
+            // Set up the connection and channel
+            busFactory = new ConnectionFactory();
+            busFactory.setUsername(userName);
+            busFactory.setPassword(password);
+            busFactory.setVirtualHost(virtualHost);
+            busFactory.setHost(hostName);
+            busFactory.setPort(port);
+            LOG.info("successfully created bus factory {}@{},  connecting", userName, hostName);
+
+            busConnection = busFactory.newConnection();
+            LOG.info("successfully created connection {}@{}, creating bus channel", userName, hostName);
+
+            busChannel = busConnection.createChannel();
+            busChannel.exchangeDeclare(EXCHANGE_NAME, "topic");
+            busChannel.queueDeclare(QUOTEREQ_QUEUE_NAME, false, false, false, null);
+            busChannel.queueDeclare(QUOTERES_QUEUE_NAME, false, false, false, null);
+            busChannel.queueDeclare(QUOTE_QUEUE_NAME, false, false, false, null);
+            busChannel.queueDeclare(TRADE_QUEUE_NAME, false, false, false, null);
+            busChannel.queueDeclare(PRICE_QUEUE_NAME, false, false, false, null);
+            LOG.info("successfully created bus channel {}@{}, binding queues", userName, hostName);
+            
+            busChannel.queueBind(QUOTEREQ_QUEUE_NAME, EXCHANGE_NAME, "QUOTEREQ.BOND.*.*");
+            busChannel.queueBind(QUOTE_QUEUE_NAME, EXCHANGE_NAME, "QUOTE.BOND.*.*");
+            busChannel.queueBind(QUOTERES_QUEUE_NAME, EXCHANGE_NAME, "QUOTERES.BOND.*.*");
+            busChannel.queueBind(TRADE_QUEUE_NAME, EXCHANGE_NAME, "TRADE.BOND.*.*");
+            busChannel.queueBind(PRICE_QUEUE_NAME, EXCHANGE_NAME, "PRICE.BOND.#");
+            isConnecting=false;
+            isConnected=true;
+            return true;
+
+        } catch(IOException | TimeoutException e){
+            LOG.error("Error connecting Bus: {}\n{}", e.getLocalizedMessage(), Utils.stackTraceToString(e));
+            tryScheduleReconnection();
+            return false;
+        }
+    }
+
+    private void tryScheduleReconnection() {
+        // connectionAttempts starts at 0
+        if (connectionAttempts >= MAX_CONNECTION_ATTEMPTS) {
+            LOG.error("Connection FAILED after too many attempts");
+            return;
+        }
+
+        // exponential backoff
+        final long backoffTimeMs = Utils.exponentialBackoffTimeMs(connectionAttempts);
+        LOG.info("Attempting reconnection in {}ms...", backoffTimeMs);
+        new Timer().schedule(new ConnectionTask(this), backoffTimeMs);
+    }
+
+    public void disconnect() {
+        if (busFactory == null || busConnection==null || busChannel == null) {
+            LOG.info("Cannot disconnect: never connected");
+            return;
+        }
+
+        try {
+            isConnected = false;
+            busConnection.close();
+            busChannel.close();
+            busFactory=null;
+        } catch (IOException | TimeoutException e) {
+            LOG.error("Cannot disconnect: {}\n{}", e.getLocalizedMessage(), Utils.stackTraceToString(e));
+        }
     }
 
     public String SendOnBus(QuoteRequest quoteRequest) throws IOException
@@ -159,5 +246,41 @@ public class MKTBusSender {
     {
         return "QUOTERES." + "BOND." + quoteResponse.getTypeValue() + "." + quoteResponse.getIssuerMemberID() + "." + quoteResponse.getFirstLeg().getSecurityID();
     }
+
+    private static class ConnectionTask extends TimerTask {
+        private final MKTBusSender busSender;
+
+        ConnectionTask(MKTBusSender busSender) {
+            this.busSender = busSender;
+        }
+
+        @Override
+        public void run() {
+            synchronized (this) {
+                isConnecting = true;
+            }
+            connectionAttempts++;
+
+            if (busSender.connect()) {
+                LOG.info("Reconnected. Issuing flush...");
+            }
+        }
+    }
+/* 
+    private static class AMQPConnectionListener implements ExceptionListener {
+        private static final Logger LOG = LoggerFactory.getLogger(AMQPConnectionListener.class);
+
+        private final AMQPClient client;
+
+        public AMQPConnectionListener(AMQPClient client) {
+            this.client = client;
+        }
+        @Override
+        public void onException(@NotNull JMSException e) {
+            LOG.error("Exception listener invoked: {}", e.getLocalizedMessage());
+            client.onConnectionLost();
+        }
+    }
+        */
 }
 
---
+++ src/main/java/org/example/Utils.java
@@ -0,0 +1,117 @@
+package org.example;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Utils {
+
+    private static final double NULL_PRICE =0.0;
+    private static final double NULL_YIELD = 9999.0;
+    private static final double NULL_SPREAD = 99999.0;
+
+   private static final DateTimeFormatter DATE_FORMATTER =
+            DateTimeFormatter.ofPattern("yyyyMMdd");
+    private static final DateTimeFormatter MTIME_FORMATTER =
+            DateTimeFormatter.ofPattern("HHmmssSSS");
+
+    private static final double RANDOM_BLANK_CHANCE = .25;
+
+    private static final double[] POW_10 = {
+            1.0,
+            10.0,
+            100.0,
+            1000.0,
+            10000.0,
+            100000.0,
+            1000000.0,
+            10000000.0,
+            100000000.0,
+            1000000000.0,
+            10000000000.0
+    };
+
+    private Utils() {
+    	// So we can't instantiate the unnecessary class
+    }
+
+    public static int getLDateToday() {
+        return Integer.parseInt(DATE_FORMATTER.format(LocalDate.now()));
+    }
+
+    public static int getMTimeNow() {
+        return Integer.parseInt(MTIME_FORMATTER.format(LocalTime.now()));
+    }
+
+    public static double getRandomPrice(double origin, double bound, double tick) {
+        if (ThreadLocalRandom.current().nextDouble() < RANDOM_BLANK_CHANCE) return NULL_PRICE;
+        return getRandomDouble(origin, bound, tick);
+    }
+
+    public static double getRandomYield(double origin, double bound) {
+        if (ThreadLocalRandom.current().nextDouble() < RANDOM_BLANK_CHANCE) return NULL_YIELD;
+        return getRandomDouble(origin, bound);
+    }
+
+    public static double getRandomSpread(double origin, double bound, double tick) {
+        if (ThreadLocalRandom.current().nextDouble() < RANDOM_BLANK_CHANCE) return NULL_SPREAD;
+        return getRandomDouble(origin, bound, tick);
+    }
+
+    public static double getRandomRate(double origin, double bound) {
+        if (ThreadLocalRandom.current().nextDouble() < RANDOM_BLANK_CHANCE) return NULL_YIELD;
+        return getRandomDouble(origin, bound);
+    }
+
+    public static double getRandomQuantity(double origin, double bound, double tick) {
+        if (ThreadLocalRandom.current().nextDouble() < RANDOM_BLANK_CHANCE) return 0.0;
+        return getRandomDouble(origin, bound, tick);
+    }
+
+    public static double getRandomDouble(double origin, double bound) {
+        return round(ThreadLocalRandom.current().nextDouble(origin, bound), 6);
+    }
+
+    public static double getRandomDouble(double origin, double bound, double tick) {
+        return roundToTick(ThreadLocalRandom.current().nextDouble(origin, bound), tick == 0 ? 0.001 : tick);
+    }
+
+    public static int getRandomInt(int origin, int bound) {
+        if (ThreadLocalRandom.current().nextDouble() < RANDOM_BLANK_CHANCE) return 0;
+        return ThreadLocalRandom.current().nextInt(origin, bound);
+    }
+
+    public static int getPrecision(double number) {
+        if (number == 0.001) {
+            return 4; // shortcut for 99.9% of real securities
+        } else if (number <= 1) {
+            return ((int) -Math.log10(number)) + 1;
+        } else {
+            return (int) Math.log10(number);
+        }
+    }
+
+    public static double round(double number, int decimals) {
+        return ((long) (number * POW_10[decimals])) / POW_10[decimals];
+    }
+
+    public static double roundToTick(double number, double tick) {
+        return round(Math.round(number / tick) * tick, getPrecision(tick));
+    }
+
+    public static String stackTraceToString(Throwable e) {
+    	  StringWriter sw = new StringWriter();
+          PrintWriter pw = new PrintWriter(sw);
+          e.printStackTrace(pw);
+          return sw.toString();
+    }
+
+    // returns an exponentially increasing multiple of 100, capped at 300000 (5 minutes)
+    public static long exponentialBackoffTimeMs(int attemptNumber) {
+        // 2^12 *100 = 409600
+        return attemptNumber > 11 ? 300000L : (long) Math.pow(2, attemptNumber) * 100L;
+    }
+}
